0baddec30639dbb5468f56b13b3974d36daec16b
Madushanka Perera <madushanka.perera@ymail.com>
2017-07-11 22:40:40 +0530

    Initial commit

---
+++ app/src/main/java/com/madushanka/imotorist/RegisterActivity.java
@@ -0,0 +1,30 @@
+package com.madushanka.imotorist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().hide();
+
+    }
+
+    public void goLogin(View v){
+        Intent i  = new Intent(this,MainActivity.class);
+        startActivity(i);
+        finishAffinity();
+
+    }
+
+}
